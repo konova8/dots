01dfb3adf08667319938a7be29fbe7d78fafe05b
konova8 <canovasimo@gmail.com>
2023-10-04 13:29:28 +0200

    Add flameshot env variables for wayland

--- other-dots/sway-nvidia.sh
+++ other-dots/sway-nvidia.sh
@@ -7,14 +7,17 @@ export VISUAL="nvim"
 # Wayland Configs
 if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
   export MOZ_ENABLE_WAYLAND=1
-  # export QT_QPA_PLATFORM=wayland
-  # export CLUTTER_BACKEND=wayland
-  # export SDL_VIDEODRIVER=wayland
 fi
 
 # Sway Configs
 if [ "$XDG_CURRENT_DESKTOP" == "sway" ]; then
   export DESKTOP_SESSION=sway
+  # For Flameshot
+  export SDL_VIDEODRIVER=wayland
+  export _JAVA_AWT_WM_NONREPARENTING=1
+  export QT_QPA_PLATFORM=wayland
+  export XDG_CURRENT_DESKTOP=sway
+  export XDG_SESSION_DESKTOP=sway
 fi
 
 sway --unsupported-gpu
