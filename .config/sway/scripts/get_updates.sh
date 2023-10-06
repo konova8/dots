dcdec22b066b1eb1211a379cc12d9698581e88fc
konova8 <canovasimo@gmail.com>
2023-10-06 09:35:09 +0200

    Make `get_updates.sh` better

--- .config/sway/scripts/get_updates.sh
+++ .config/sway/scripts/get_updates.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 res=''
 
-sleep 5
+sleep 15
+rescheckupdates=$(checkupdates)
 
-updates=$(checkupdates | wc -l)
-res+='Updates: '
-res+=$updates
-res+=$'\n\n'
+if [ $? -eq 0 ]; then
+  updates=$(echo $rescheckupdates | wc -l)
+  res+='Updates: '
+  res+=$updates
+  res+=$'\n\n'
 
-aur=$(yay -Qum | wc -l)
-res+='AUR: '
-res+=$aur
+  aur=$(yay -Qum | wc -l)
+  res+='AUR: '
+  res+=$aur
 
-notify-send -u critical "$res"
+  notify-send -u critical "$res"
+else
+  notify-send -u critical "Not yet connected"
+fi
