705b92d502b81b598261e1d92de1cecbfd980be2
Simone Canova <canovasimo@gmail.com>
2023-12-08 14:28:35 +0100

    Fix Lualine typo for dependencies

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
     opts = {
         options = {
             icons_enabled = false,
