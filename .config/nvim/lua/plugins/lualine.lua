fba7054cc5efae0e4065e38e7303eec1200821a4
Simone Canova <canovasimo@gmail.com>
2023-12-10 17:16:31 +0100

    Functioning icons with nvim

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,10 +2,10 @@ return {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
-    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+    dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
     opts = {
         options = {
-            icons_enabled = false,
+            icons_enabled = true,
             theme = 'gruvbox',
             component_separators = '|',
             section_separators = '',
