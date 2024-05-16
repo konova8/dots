2005090f21c9c541c94d850a99436e89af7777f3
Simone Canova <canovasimo@gmail.com>
2024-05-16 12:42:33 +0200

    Fix not starting telescope for symbols

--- .config/nvim/lua/plugins/telescope-symbols.lua
+++ .config/nvim/lua/plugins/telescope-symbols.lua
@@ -4,4 +4,7 @@ return
     keys = {
         { "<leader>ts", "<cmd>Telescope symbols<cr>", desc = "[T]elescope [S]ymbols" }
     },
+    config = function()
+        require('telescope')
+    end,
 }
