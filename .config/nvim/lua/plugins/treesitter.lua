8b752c9a8dfae7da626e3907306bbd74be59eeaf
Simone Canova <canovasimo@gmail.com>
2024-05-18 15:32:24 +0200

    Fix treesitter latex highlighting bug

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -14,6 +14,8 @@ return
             -- Add languages to be installed here that you want installed for treesitter
             ensure_installed = { 'c', 'cpp', 'go', 'javascript', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
+            ignore_install = { "latex" },
+
             -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
             auto_install = true,
 
